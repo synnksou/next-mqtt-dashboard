77627b7f1551b0c5f4c22802dff8edc5de77ffaa
Antoine Heinrich <antoine.h@tymate.com>
2023-06-09 18:13:50 +0200

    feat: init project uwu

--- config/site.ts
+++ config/site.ts
@@ -2,17 +2,11 @@ export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
   name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  description: "Dashboard with Mqtt and Next.js",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
 }
